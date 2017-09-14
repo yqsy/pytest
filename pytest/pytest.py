b30b55320a609b5f190e38e2f8de74e7b2f86d7f
yqsy <yqsy021@126.com>
2017-09-14 22:20:47 +0800

    cool

--- pytest/pytest.py
+++ pytest/pytest.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+import traceback
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 
@@ -12,7 +13,33 @@ logging.basicConfig(
     format='%(asctime)s %(thread)d %(levelname)s %(filename)s:%(lineno)d %(message)s',
 )
 
+def f():
+    logging.info('calling method g() in f()')
+    return g()
+
+def g():
+    logging.info('calling method h() in g()')
+    return h()
+
+def h():
+    logging.info('calling method i() in h()')
+    return i()
+
+def i():
+    logging.info('append element i to gList in i()')
+
+    raise Exception('exception has happen')
+
+
 if __name__ == '__main__':
     hello()
 
     logging.debug('Information during calling f()')
+
+    try:
+        f()
+    except Exception as ex:
+        ty,tv,tb = sys.exc_info()
+        logging.critical('{} {}'.format(ty,tv))
+        logging.critical(''.join(traceback.format_tb(tb)))
+        sys.exit(1)
