724a97b1f70995f5fb4225af84163f6c05efca48
Guochun.Gong <yqsy021@126.com>
2017-09-20 12:24:36 +0800

    contextlib.contextmanager 实现上下文管理

--- pytest/test_context.py
+++ pytest/test_context.py
@@ -1,3 +1,7 @@
+import contextlib
+
+
+# 这个应该是传统的做法,重载两个函数
 class LookingGlass:
     def __enter__(self):
         import sys
@@ -23,6 +27,26 @@ class LookingGlass:
             return True
 
 
+@contextlib.contextmanager
+def looking_glass():
+    import sys
+    original_write = sys.stdout.write
+
+    def reverse_write(text):
+        original_write(text[::-1])
+
+    sys.stdout.write = reverse_write
+    msg = ''
+    try:
+        yield 'JABBERWOCKY'
+    except ZeroDivisionError:
+        msg = 'Please DO NOT divide by zero!'
+    finally:
+        sys.stdout.write = original_write
+        if msg:
+            print(msg)
+
+
 def main():
     # with 会调用 __enter__ 以及 __exit__
     with LookingGlass() as what:
@@ -35,6 +59,9 @@ def main():
     manager.__exit__(None, None, None)
     print(monster)
 
+    # contextlib.contextmanager 上下文管理
+    with looking_glass() as what:
+        print(what)
 
 if __name__ == '__main__':
     main()
