54bd069605893ced840442c398ee4ab0c2e5dd7d
Guochun.Gong <yqsy021@126.com>
2017-09-20 17:32:27 +0800

    协程throw异常

--- pytest/test_yield.py
+++ pytest/test_yield.py
@@ -65,8 +65,49 @@ def main3():
     print(coro_avg.send(30))
 
 
+class DemoException(Exception):
+    """
+    为演示定义的异常类型
+    """
+
+
+def demo_exec_handleing():
+    print('-> coroutine started')
+    while True:
+        try:
+            x = yield
+        except DemoException:
+            print('*** DemoException handled, Continuing...')
+        else:
+            print('-> coroutine received: {!r}'.format(x))
+
+    raise RuntimeError('This line should never run.')
+
+
+def main4():
+    exc_coro = demo_exec_handleing()
+
+    next(exc_coro)
+
+    exc_coro.send(11)
+    exc_coro.send(22)
+
+    # 停止协程
+    # exc_coro.close()
+    # from inspect import getgeneratorstate
+    # print(getgeneratorstate(exc_coro))
+
+
+    # 传入这个异常不会停止协程
+    exc_coro.throw(DemoException)
+
+
+    # 传这个异常就有问题
+    # exc_coro.throw(Exception)
+
 
 if __name__ == '__main__':
     # main1()
     # main2()
-    main3()
+    # main3()
+    main4()
