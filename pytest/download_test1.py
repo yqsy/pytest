9974d3487c5ef9f1aee48aa38f14e6cd5a550da0
Guochun.Gong <yqsy021@126.com>
2017-09-21 10:29:06 +0800

    extract一下,因为涉及到复用

--- pytest/download_test1.py
+++ pytest/download_test1.py
@@ -13,28 +13,40 @@ BASE_URL = 'http://flupy.org/data/flags'
 DEST_DIR = 'downloads/'
 
 
-def main():
+def get_flag(cc):
+    url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):
+    print(text, end=' ')
+    sys.stdout.flush()
+
+
+def save_flag(image, cc):
+    path = os.path.join(DEST_DIR, cc.lower() + '.gif')
+    with open(path, 'wb') as fp:
+        fp.write(image)
+
+
+def download_many(cc_list):
+    for cc in sorted(POP20_CC):
+        image = get_flag(cc)
+        show(cc)
+        save_flag(image, cc)
+    return len(cc_list)
+
+
+def main(download_many):
     if not os.path.exists(DEST_DIR):
         os.makedirs(DEST_DIR)
-
     t0 = time.time()
-
-    for cc in sorted(POP20_CC):
-        url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
-        resp = requests.get(url)
-        image = resp.content
-        print(cc, end=' ')
-        sys.stdout.flush()
-        path = os.path.join(DEST_DIR, cc.lower() + '.gif')
-        with open(path, 'wb') as fp:
-            fp.write(image)
-
-    count = len(POP20_CC)
+    count = download_many(POP20_CC)
     elapsed = time.time() - t0
-
     msg = '\n{} flags downloaded in {:.2f}s'
     print(msg.format(count, elapsed))
-    
+
 
 if __name__ == '__main__':
-    main()
+    main(download_many)
