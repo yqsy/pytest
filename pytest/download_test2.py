6f5c9de35db07e22f7754ea080f8af4cd5442c00
Guochun.Gong <yqsy021@126.com>
2017-09-21 14:03:40 +0800

    期物,不太好理解的

--- pytest/download_test2.py
+++ pytest/download_test2.py
@@ -1,5 +1,5 @@
 import sys
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from pytest.download_test1 import get_flag, show, save_flag, main
 
@@ -22,5 +22,25 @@ def download_many(cc_list):
     return len(list(res))
 
 
+def download_many2(cc_list):
+    cc_list = cc_list[:5]
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+
+        results = []
+        for future in as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+
+    return len(results)
+
+
 if __name__ == '__main__':
-    main(download_many)
+    main(download_many2)
