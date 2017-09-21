a6d64606bfe1ab3be43cdea2bd11f14ff91bb7b1
Guochun.Gong <yqsy021@126.com>
2017-09-21 10:43:28 +0800

    ThreadPoolExecutor 速度很快,不过抽象需要仔细研究一下才能弄懂

---
+++ pytest/download_test2.py
@@ -0,0 +1,25 @@
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+from pytest.download_test1 import get_flag, show, save_flag, main
+
+MAX_WORKERS = 20
+
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc)
+    return cc
+
+
+def download_many(cc_list):
+    workers = min(MAX_WORKERS, len(cc_list))
+    with ThreadPoolExecutor(workers) as executor:
+        res = executor.map(download_one, sorted(cc_list))
+
+    return len(list(res))
+
+
+if __name__ == '__main__':
+    main(download_many)
